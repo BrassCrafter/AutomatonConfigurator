328bb7ca617763b55ec8cb7ab41659c03986c6cc
t <tillmann-rohlfing@protonmail.com>
2024-03-01 23:08:42 +0100

    map
    Took 24 minutes

--- src/Map.java
+++ src/Map.java
@@ -1,6 +1,11 @@
 public class Map {
-    Object[][] map;
-    public Map(int lengthOfAlphabet, int lengthOfStates){
-        map = new Object[lengthOfStates][lengthOfAlphabet];
+    List<State>[][] map;
+    public Map(int lengthOfStates, int lengthOfAlphabet){
+        map = new List[lengthOfStates][lengthOfAlphabet];
     }
+    public void addAssoziation(int indexOfState, int indexOfAlphabet, State assoziatedState){
+        map[indexOfState][indexOfAlphabet].append(assoziatedState);
+    }
+    public List<State> getAssoziation(int indexOfState, int indexOfAlphabet)
+
 }
