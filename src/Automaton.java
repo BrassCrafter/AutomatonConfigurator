6d14a2fb86d2a180d39e47ec5cf24510d23aa00e
t <tillmann-rohlfing@protonmail.com>
2024-03-02 15:47:15 +0100

    fertigstellung
    Took 15 minutes

--- src/Automaton.java
+++ src/Automaton.java
@@ -1,9 +1,29 @@
 public class Automaton {
     Alphabet alphabet;
     Map map;
-    public Automaton(Map map, Alphabet alphabet){
+    String word;
+    List<State> activeStates;
+    public Automaton(Map map, Alphabet alphabet, State start){
+        activeStates = new List<>();
+        activeStates.append(start);
         this.map = map;
         this.alphabet = alphabet;
+    }
+    public void checkWord(String word){
+        this.word = word;
+        while(word != ""){
+
+            char zeichen = getCurrentChar();
+            for(activeStates.toFirst(); activeStates.hasAccess(); activeStates.next()){
+
+            }
+
+        }
+    }
+    private char getCurrentChar() {
+        char first = word.charAt(0);
+        word = word.substring(1);
+        return first;
     }
 
     public static void main(String[] args) {
